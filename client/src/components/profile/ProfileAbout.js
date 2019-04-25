e914513dcedffd65d4e734b3e784cc52db44b5ce
sanchez1179 <icsanchez1179@gmail.com>
2019-04-25 00:16:20 -0700

    minor ui error on the header of the bio within profile

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -20,7 +20,7 @@ class ProfileAbout extends Component {
       <div className="row">
         <div className="col-md-12">
           <div className="card card-body bg-light mb-3">
-            <h3 className="text-center text-info">{firstName} is Bio</h3>
+            <h3 className="text-center text-info">{firstName}'s Bio</h3>
             <p className="lead">
               {isEmpty(profile.bio) ? (
                 <span>{firstName} does not have a bio</span>
